a0db5cc9c41314ffd6251d9f276b8855df5d9bd7
BuildTools <unconfigured@null.spigotmc.org>
2020-08-12 23:11:08 +0200

    Updated

---
+++ js/chat.js
@@ -0,0 +1,25 @@
+var options = document.querySelectorAll('.menuItem');
+
+for (let i = 0; i < options.length; i++) {
+    options[i].addEventListener("click", function(e) {
+        let target = options[i].getAttribute('href');
+        document.querySelector('div.active').classList.remove('active');
+        document.querySelector('a.active').classList.remove('active');
+        options[i].classList.add('active');
+        document.querySelector(target).classList.add('active');  
+    });
+}
+
+
+
+
+
+window.onload = (event) => {
+    
+
+
+
+
+};
+
+
