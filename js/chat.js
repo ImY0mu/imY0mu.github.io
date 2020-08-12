8c39e4356c0233877c0eb28c694be99885ea30be
BuildTools <unconfigured@null.spigotmc.org>
2020-08-12 23:14:01 +0200

    a

--- js/chat.js
+++ js/chat.js
@@ -2,6 +2,7 @@ var options = document.querySelectorAll('.menuItem');
 
 for (let i = 0; i < options.length; i++) {
     options[i].addEventListener("click", function(e) {
+        e.preventDefault();
         let target = options[i].getAttribute('href');
         document.querySelector('div.active').classList.remove('active');
         document.querySelector('a.active').classList.remove('active');
