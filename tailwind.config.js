5c66b52972495d360ea86e3210bb3bc56f1f6eaf
ImY0mu <justy0mu@gmail.com>
2021-09-16 14:44:22 +0200

    test

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber,
+      green: colors.emerald
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+//npx tailwindcss -o tailwind.css
